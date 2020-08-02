e01837a68d9844d899659bc6e816fa3138b783e0
Paul Krohn <pkrohn@daemonize.com>
2020-08-02 09:27:41 -0700

    add syslog

--- speakerswitcher/stuff.py
+++ speakerswitcher/stuff.py
@@ -1,3 +1,8 @@
+import syslog
+from syslog import LOG_DEBUG as DEBUG, LOG_INFO as INFO  # LOG_WARNING as WARNING, LOG_ERR as ERROR
+import os
+import sys
+
 import redis
 from gpiozero.output_devices import DigitalOutputDevice
 
@@ -9,18 +14,23 @@ class Listener(object):
         self.r = redis.Redis()
         self.p = self.r.pubsub(ignore_subscribe_messages=True)
         self.pin = DigitalOutputDevice(pin)
+        syslog.openlog(
+            ident=os.path.basename(sys.argv[0]),
+            facility=syslog.LOG_USER
+        )
+        self.log = syslog.syslog
 
     def message_handler(self, message):
-        print('message: {}'.format(message))
+        self.log(DEBUG, 'message: {}'.format(message))
         if message['data'] == b'start':
             self.pin.on()
         else:
             self.pin.off()
 
     def run(self):
+        self.log(INFO, "listener started")
         self.p.subscribe(**{'play': self.message_handler})
-
-        thread = self.p.run_in_thread(sleep_time=0.01)
+        self.p.run_in_thread(sleep_time=0.01)
 
 
 def listener():
